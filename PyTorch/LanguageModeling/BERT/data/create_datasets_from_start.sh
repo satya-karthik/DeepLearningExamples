a6fd451f76e2415d9c473eeb44b4e0f423e98d73
Satya karthik Ubiq server <satya.karthik@sony.com>
2021-12-05 11:37:10 +0000

    commented out the code for download, format and sharding as we already have sharded data

--- PyTorch/LanguageModeling/BERT/data/create_datasets_from_start.sh
+++ PyTorch/LanguageModeling/BERT/data/create_datasets_from_start.sh
@@ -13,35 +13,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-to_download=${1:-"wiki_only"}
+# to_download=${1:-"wiki_only"}
 
-#Download
-if [ "$to_download" = "wiki_books" ] ; then
-    python3 /workspace/bert/data/bertPrep.py --action download --dataset bookscorpus
-fi
+# #Download
+# if [ "$to_download" = "wiki_books" ] ; then
+#     python3 /workspace/bert/data/bertPrep.py --action download --dataset bookscorpus
+# fi
 
-python3 /workspace/bert/data/bertPrep.py --action download --dataset wikicorpus_en
-python3 /workspace/bert/data/bertPrep.py --action download --dataset google_pretrained_weights  # Includes vocab
-python3 /workspace/bert/data/bertPrep.py --action download --dataset squad
-python3 /workspace/bert/data/bertPrep.py --action download --dataset mrpc
-python3 /workspace/bert/data/bertPrep.py --action download --dataset sst-2
+# python3 /workspace/bert/data/bertPrep.py --action download --dataset wikicorpus_en
+# python3 /workspace/bert/data/bertPrep.py --action download --dataset google_pretrained_weights  # Includes vocab
+# python3 /workspace/bert/data/bertPrep.py --action download --dataset squad
+# python3 /workspace/bert/data/bertPrep.py --action download --dataset mrpc
+# python3 /workspace/bert/data/bertPrep.py --action download --dataset sst-2
 
-# Properly format the text files
-if [ "$to_download" = "wiki_books" ] ; then
-    python3 /workspace/bert/data/bertPrep.py --action text_formatting --dataset bookscorpus
-fi
-python3 /workspace/bert/data/bertPrep.py --action text_formatting --dataset wikicorpus_en
+# # Properly format the text files
+# if [ "$to_download" = "wiki_books" ] ; then
+#     python3 /workspace/bert/data/bertPrep.py --action text_formatting --dataset bookscorpus
+# fi
+# python3 /workspace/bert/data/bertPrep.py --action text_formatting --dataset wikicorpus_en
 
-if [ "$to_download" = "wiki_books" ] ; then
-    DATASET="books_wiki_en_corpus"
-else
-    DATASET="wikicorpus_en"
-    # Shard the text files
-fi
+# if [ "$to_download" = "wiki_books" ] ; then
+#     DATASET="books_wiki_en_corpus"
+# else
+#     DATASET="wikicorpus_en"
+#     # Shard the text files
+# fi
 
-# Shard the text files
-python3 /workspace/bert/data/bertPrep.py --action sharding --dataset $DATASET
+# # Shard the text files
+# python3 /workspace/bert/data/bertPrep.py --action sharding --dataset $DATASET
 
+DATASET="books_wiki_en_corpus"
 # Create HDF5 files Phase 1
 python3 /workspace/bert/data/bertPrep.py --action create_hdf5_files --dataset $DATASET --max_seq_length 128 \
 --max_predictions_per_seq 20 --vocab_file $BERT_PREP_WORKING_DIR/download/google_pretrained_weights/uncased_L-24_H-1024_A-16/vocab.txt --do_lower_case 1
