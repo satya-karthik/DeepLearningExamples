08a4f9d54ce06dba8f302bcf9d21ba3477b6d183
Satya karthik Ubiq server <satya.karthik@sony.com>
2021-12-20 20:30:34 +0900

    updated training scripts

--- PyTorch/LanguageModeling/BERT/scripts/run_pretraining_large.sh
+++ PyTorch/LanguageModeling/BERT/scripts/run_pretraining_large.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 echo "Container nvidia build = " $NVIDIA_BUILD_ID
-train_batch_size=${1:-8192}
+train_batch_size=${1:-16384}
 learning_rate=${2:-"6e-3"}
 precision=${3:-"fp16"}
 num_gpus=${4:-4}
@@ -24,23 +24,23 @@ save_checkpoint_steps=${7:-1600}
 resume_training=${8:-"false"}
 create_logfile=${9:-"true"}
 accumulate_gradients=${10:-"true"}
-gradient_accumulation_steps=${11:-32}
+gradient_accumulation_steps=${11:-128}
 seed=${12:-12439}
-job_name=${13:-"bert_base_lamb_pretraining"}
+job_name=${13:-"bert_large_lamb_pretraining"}
 allreduce_post_accumulation=${14:-"true"}
 allreduce_post_accumulation_fp16=${15:-"true"}
 train_batch_size_phase2=${16:-8192}
 learning_rate_phase2=${17:-"4e-3"}
 warmup_proportion_phase2=${18:-"0.128"}
 train_steps_phase2=${19:-10} # 9042
-gradient_accumulation_steps_phase2=${20:-256}
+gradient_accumulation_steps_phase2=${20:-512}
 DATASET=hdf5_lower_case_1_seq_len_128_max_pred_20_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5/books_wiki_en_corpus # change this for other datasets
 DATA_DIR_PHASE1=${21:-$BERT_PREP_WORKING_DIR/${DATASET}/}
 DATASET2=hdf5_lower_case_1_seq_len_512_max_pred_80_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5/books_wiki_en_corpus # change this for other datasets
 DATA_DIR_PHASE2=${22:-$BERT_PREP_WORKING_DIR/${DATASET2}/}
 CODEDIR=${23:-"/workspace/bert"}
 init_checkpoint=${24:-"None"}
-BERT_CONFIG=bert_config_base.json
+BERT_CONFIG=bert_config_large.json
 RESULTDIR=${25:-"/results"}
 RUN_TIME=$(date +"%Y_%m_%d_%H_%M_%S")
 RESULTS_DIR=$RESULTDIR/$job_name/$RUN_TIME
@@ -153,7 +153,7 @@ set +x
 
 echo "finished pretraining"
 
-#Start Phase2
+# Start Phase2
 
 PREC=""
 if [ "$precision" = "fp16" ] ; then
